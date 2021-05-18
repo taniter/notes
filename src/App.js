688048e0eac868c16357acbe5392a16b3abd4831
taniter <thanhtanit.cntt@gmail.com>
2021-05-18 21:23:55 +0700

    2

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="list_notes">
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+          <div className="note m-1 d-flex p-2 bg-white">
+            <h6 className="time">2/5/2021</h6>
+            <h6 className="ml-3 content">c39 CThanh Thay Main39 CThanh Thay Main39 CThanh Thay Main</h6>
+            <div className="fa fa-times-circle text-danger delete"></div>
+          </div>
+        </div>
+        <div class="form-group text-input">
+          <input type="text" className="form-control mt-2" name="text-input" id="text-input" placoehlder="text..." />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
